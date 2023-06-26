7f7d527cc2f74efbaa64164b35ff27d0d841cf6a
Petya Babukova <p_babukova@abv.bg>
2023-06-26 15:13:18 +0300

    Change aboutController to homeController

--- routes.js
+++ routes.js
@@ -1,15 +1,15 @@
 const{ Router } = require('express');
 const productController = require('./controlers/productController');
-const aboutController = require('./controlers/aboutController')
+const homeController = require('./controlers/homeController')
 
 const router = Router();
 
-router.use('/about', aboutController);
-router.use('/', productController);
+router.use('/', homeController);
+router.use('/products', productController);
 
 router.get('*', (req, res)=>{
     res.render('404')
-})
+});
 
 
 module.exports = router;
