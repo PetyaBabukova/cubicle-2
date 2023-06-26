1c75a092dda166ed82e5a1d3f32794c0288ed502
Petya Babukova <p_babukova@abv.bg>
2023-06-26 14:54:27 +0300

    Add layouts and main page

--- controlers/productController.js
+++ controlers/productController.js
@@ -3,11 +3,11 @@ const router = Router();
 
 //Method notation sintaxys - thes could be written as anonimus func, arrow func, etc.
 router.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
 });
 
 router.get('/create', (req, res) => {
-    res.render('create', { layout: false });
+    res.render('create');
 })
 
 
